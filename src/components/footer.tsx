0f916e0e3ff3475f83f380e3a1a645e2ccdd1223
bharat-kr-dev <bharatkushwaha7112005.5@gmail.com>
2025-10-16 10:36:34 +0530

    add image

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -170,7 +170,7 @@ const Footer: React.FC = () => {
           
           {/* Copyright */}
           <div className="text-sm text-gray-400">
-            © {currentYear} Raiments. All rights reserved.
+            © {currentYear} appral. All rights reserved.
           </div>
 
           {/* Legal Links */}
